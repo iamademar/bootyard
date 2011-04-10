266a6dad47414a402634be21cf4fb8f6edfe07a8
Ademar <ademar@ademar-laptop.(none)>
2011-04-11 04:25:40 +0800

    added new logo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bootyard::Application.config.secret_token = 'efa79d34b6612f6d8683a89d592421dc9317b56d37bef6b9e49d53ae55b11ccf8dc686711b09893058a071e2acef6d1807802451ca859b9f33786ff2237b08f7'
