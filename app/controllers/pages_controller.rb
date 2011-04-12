eb80ce66b2a137355a9a2f2d00ce05ec03c4f69c
Ademar <ademar@ademar-laptop.(none)>
2011-04-12 12:29:32 +0800

    removed portfolio link; added gerda's profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,7 @@ class PagesController < ApplicationController
   def index
 
   end
+
+  def portfolio
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Bootyard::Application.routes.draw do
-  resources :pages
+  resources :pages do
+    collection do
+      get 'portfolio'
+    end
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
