ada429104a16d8d0d5df74ce4db7c42a03f1a5b5
Gerda Decio <gerdadecio@gmail.com>
2011-04-16 16:17:54 +0800

    intializer

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -6,8 +6,6 @@ Compass::AppIntegration::Rails.initialize!
 
 FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
 
-Compass::AppIntegration::Rails.initialize!
-
 Rails.configuration.middleware.delete('Sass::Plugin::Rack')
 Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
 
